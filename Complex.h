5b07a59440d9a261e782223d2e35c17cb369cc9e
eatpopminh <54875208+eatpopminh@users.noreply.github.com>
2019-11-24 23:07:16 -0800

    Some more header stuff

--- Complex.h
+++ Complex.h
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ class Complex
 		int re;		//real part of a complex number
 		int im;		//imaginary part of a complex number
 		int dem;	//denominator part of a complex number
-		string toString() cosnt;	//Format [re + imi]/dem
+		string toString() const;	//Format [re + imi]/dem
 		
 };
 
@@ -26,4 +27,27 @@ Complex :: Complex(int r, int i, int d)
 	im = i;
 	dem = d;
 }
+//string Complex :: toString() const
+//{
+//	string temp = "[" + static_cast<char>(re); //+ " + " ;//+ static_cast<char>(im) + "i]/" + static_cast<char>(dem);
+//	return temp;
+//}
 
+//Overloading operator for easy arithmetic
+Complex operator+(Complex a, Complex b);
+Complex operator-(Complex a, Complex b);
+Complex operator*(Complex a, Complex b);
+Complex operator/(Complex a, Complex b);
+ostream& operator<<(ostream& stream, Complex a);
+
+Complex operator+(Complex a, Complex b)
+{
+	Complex com(a.re + b.re, a.im + b.im, 1);
+	com.toString();
+
+}
+
+
+//Find GCD and LCM to reduce fraction and add fraction
+int gcd(int a, int b);
+int lcm(int a, int b);
--- Main.cpp
+++ Main.cpp
@@ -2,33 +2,38 @@
 #include <string>
 #include "DoublyLinkedList.h"
 #include "Node.h"
+#include "Complex.h"
 using namespace std;
 
 int main()
 {
-	DoublyLinkedList<int> dll;
-//	Node<int>* iter = dll.getHead();
-//	while(count < 5)
-//	{
-//		iter = iter -> next;
-//		count++;
-//	}
-//	ten->data = 10;
-//	dll.addNode(10,iter);
+//	DoublyLinkedList<int> dll;
+////	Node<int>* iter = dll.getHead();
+////	while(count < 5)
+////	{
+////		iter = iter -> next;
+////		count++;
+////	}
+////	ten->data = 10;
+////	dll.addNode(10,iter);
+//	
+//	int a = 6;
+//	
+//	dll.addFront(7);
+//	dll.addFront(a);
+//	
+//	//dll.popFront();
+//	//dll.addBack(8);
+//	//dll.popBack();
+//	//dll.addNode(100,ten);
+//	
+//	cout<<dll.getSize()<<endl;
+//	cout<<dll.isEmpty()<<endl;
+//	dll.displayList();
 	
-	int a = 6;
+	Complex com(3,4,1);
+	//cout<<com.toString()<<endl;
 	
-	dll.addFront(7);
-	dll.addFront(a);
-	
-	//dll.popFront();
-	//dll.addBack(8);
-	//dll.popBack();
-	//dll.addNode(100,ten);
-	
-	cout<<dll.getSize()<<endl;
-	cout<<dll.isEmpty()<<endl;
-	dll.displayList();
 	
 	return 0;
 }
